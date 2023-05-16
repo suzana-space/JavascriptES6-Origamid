44e1d556d3f2c853dedaa5b39ef8bf363467334b
suzana <suzana@adxspace.com.br>
2023-05-16 18:28:26 -0300

    objetos é tudo no js

---
+++ Javascript/JavascriptES6-02/Objetos-03.js
@@ -0,0 +1,11 @@
+//tudo é objeto
+
+let PessoaSuzana = {
+    nome: 'Suzana',
+    idade: 19,
+    salario: 735.909898,
+}
+console.log(PessoaSuzana.nome.toLocaleUpperCase())
+console.log(PessoaSuzana.idade)
+console.log(PessoaSuzana.salario.toFixed(1))
+
