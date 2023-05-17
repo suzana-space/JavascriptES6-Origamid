d07523cc4436ed51fc61dfb1d8ccd7d43e20c95d
suzana <suzana@adxspace.com.br>
2023-05-17 16:47:22 -0300

    atribuicoes

---
+++ Javascript/JavascriptES6-02/Atribuicoes.js
@@ -0,0 +1,25 @@
+//Operadores de atribuição funcionam de formas abreviadas: 
+//sempre antes do operador de igualdade 
+let a = 5
+let b = 8
+let resultado
+
+resultado = a += b  //a = a + b
+console.log(resultado)
+
+resultado = a -= b  //a = a - b
+console.log(resultado)
+
+resultado = a *= b  //a = a * b
+console.log(resultado)
+
+resultado = a /= b  //a = a / b
+console.log(resultado)
+
+resultado = a %= b  //a = a % b
+console.log(resultado)
+
+resultado = a **= b //a = a ** b
+console.log(resultado)
+
+
