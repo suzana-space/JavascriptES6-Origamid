f9a3ab7f8a520ce0beaf106adc60e58c57140f55
suzana <suzana@adxspace.com.br>
2023-05-22 17:49:31 -0300

    arrays e objetos

---
+++ Javascript/JavascriptES6-02/Objetos-04.js
@@ -0,0 +1,16 @@
+const carros = new Array('ford', 'chevrole','jeep','fusca')
+carros[10] = 'moto'
+console.log(carros.length)
+console.log(carros)
+
+//Array.of() cria um array 
+novoArray = Array.of(10)
+console.log(novoArray)
+
+const comidas = ['banana','pão','figado',['agua','refri','limonada',['jujuba','pirulito']]]
+console.log(comidas[3])
+console.log(comidas.length)
+
+
+
+
